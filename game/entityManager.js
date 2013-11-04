79c6ac9eaef66fbac21685f34700dfd9cb45e579
Haukur Óskar Þorgeirsson <hawkuro@gmail.com>
2013-11-04 19:53:08 +0000

    View ready with straight line bottom for now

--- game/entityManager.js
+++ game/entityManager.js
@@ -178,11 +178,15 @@ render: function(ctx) {
         ctx.translate(-s.cx - g_canvas.width/2,-s.cy - g_canvas.height/2); 
 	//console.log((s.cx) + " "  + (s.cy));
     }
+    
     ctx.strokeStyle="white";
     ctx.beginPath();
-    ctx.rect(g_canvas.width,g_canvas.height,g_canvas.width,g_canvas.height);
+    //ctx.rect(g_canvas.width,g_canvas.height,g_canvas.width,g_canvas.height);
+    ctx.moveTo(s.cx - g_canvas.width/2, g_canvas.height*2);
+    ctx.lineTo(s.cy + 5*g_canvas.width/2, g_canvas.height*2);
     ctx.stroke();
     ctx.closePath();
+    
     for (var c = 0; c < this._categories.length; ++c) {
 
         var aCategory = this._categories[c];
