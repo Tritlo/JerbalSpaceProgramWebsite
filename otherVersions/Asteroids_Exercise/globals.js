c818896c52db078e69b6c112cb8dc349f5a09589
tritlo <mpg@mpg.is>
2013-11-04 09:41:18 +0000

    made a main version, so that things don't get out of hand

---
+++ otherVersions/Asteroids_Exercise/globals.js
@@ -0,0 +1,23 @@
+// =======
+// GLOBALS
+// =======
+/*
+
+Evil, ugly (but "necessary") globals, which everyone can use.
+
+*/
+
+"use strict";
+
+/* jshint browser: true, devel: true, globalstrict: true */
+
+var g_canvas = document.getElementById("myCanvas");
+var g_ctx = g_canvas.getContext("2d");
+
+// The "nominal interval" is the one that all of our time-based units are
+// calibrated to e.g. a velocity unit is "pixels per nominal interval"
+//
+var NOMINAL_UPDATE_INTERVAL = 16.666;
+
+// Multiply by this to convert seconds into "nominals"
+var SECS_TO_NOMINALS = 1000 / NOMINAL_UPDATE_INTERVAL;
