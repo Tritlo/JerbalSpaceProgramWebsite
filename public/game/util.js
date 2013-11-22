5ecdc8497cb6080f4b116187b81d7ad020782102
tritlo <mpg@mpg.is>
2013-11-22 15:48:35 +0000

    fixed mouse in menu bug

--- public/game/util.js
+++ public/game/util.js
@@ -47,17 +47,9 @@ circInBox: function (x,y,radius,topLeftCorner,bottomRightCorner){
 },
 
 findPos : function(obj) {
-    var curleft = 0, curtop = 0;
-    if (obj.offsetParent) {
-        do {
-            curleft += obj.offsetLeft;
-            curtop += obj.offsetTop;
-            } while (obj = obj.offsetParent);
-        return { x: curleft, y: curtop };
-    }
-    return undefined;
+    var rect = obj.getBoundingClientRect();
+    return {x: rect.left, y: rect.top}
 },
-
 
 // RANDOMNESS
 // ==========
