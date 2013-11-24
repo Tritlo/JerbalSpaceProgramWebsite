08e1cc4d0f0f900bafccd672f44b267f9e37fcbc
tritlo <mpg@mpg.is>
2013-11-24 05:02:24 +0000

    rename works

--- client/game/game.js
+++ client/game/game.js
@@ -1,15 +1,15 @@
 Template.game.created = function() {
     var gameFiles = [
-	    "game/data.js","game/csscolorparser.js" , "game/globals.js" , "game/consts.js"
-	  , "game/util.js" , "game/keys.js" , "game/spatialManager.js"
-	  , "game/Stars.js" , "game/entityManager.js" 
-	  , "game/Entity.js" , "game/Terrain.js" , "game/Ship.js" 
-	  , "game/update.js" , "game/render.js" , "game/HUD.js"
-	  , "game/main.js" , "game/State.js", "game/settings.js"
-	  , "game/Grid.js" , "game/Part.js" , "game/Menu.js", "game/MainMenu.js"
-	  , "game/PartsDesigner.js" , "game/ShipDesigner.js" , "game/Simulation.js"
-	  , "game/Viewer.js" , "game/stateManager.js" , "game/handleMouse.js"
-	  ,"game/JerbalSpaceProgram.js", "game/start.js"
+	"game/01-csscolorparser.js" , "game/02-data.js" , "game/03-globals.js"
+       , "game/04-consts.js" , "game/05-keys.js" , "game/06-settings.js"
+       , "game/07-util.js" , "game/08-update.js" , "game/09-render.js"
+       , "game/10-handleMouse.js" , "game/11-main.js" , "game/12-State.js"
+       , "game/13-stateManager.js" , "game/14-spatialManager.js" , "game/15-Stars.js"
+       , "game/16-Entity.js" , "game/17-entityManager.js" , "game/18-Terrain.js"
+       , "game/19-Ship.js" , "game/20-HUD.js" , "game/21-Grid.js"
+       , "game/22-Part.js" , "game/23-Menu.js" , "game/24-MainMenu.js"
+       , "game/25-PartsDesigner.js" , "game/26-ShipDesigner.js" , "game/27-Viewer.js"
+       , "game/28-Simulation.js" , "game/29-JerbalSpaceProgram.js" , "game/30-start.js"
 	];
     gameFiles = gameFiles.map(function(s){
 	return Meteor.absoluteUrl(s);
