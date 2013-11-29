38a2c7df91f88cc036cc4b2abf9a6f129a578ff6
solrun <solalola@gmail.com>
2013-11-29 01:27:10 +0000

    fixed pagination for personal browsing

--- client/parts/myParts/myPartsList/myPartsList.js
+++ client/parts/myParts/myPartsList/myPartsList.js
@@ -1,7 +1,7 @@
 Template.myPartsList.helpers({
 		myParts: function(){
         var page = parseInt(Session.get('currentPage'));
-        var limit = parseInt(Session.get('paginationLimit'));
+        var limit = parseInt(Session.get('myPaginationLimit'));
         return Parts.find({ author:Meteor.user().username },{skip: (page-1)*limit, limit: limit});
 	}
 });
@@ -10,6 +10,6 @@ Template.myPartsList.helpers({
 Template.myPartsList.created = function(){
     var page = parseInt(Session.get('currentPage'));
     var totalParts = Meteor.call("myPartsCount", function(error,result){ Session.set("paginationTotal",result);} );
-    Session.set("paginationLimit",3);
-    Session.set("paginationOf","Part");
+    Session.set("myPaginationLimit",3);
+    Session.set("myPaginationOf","myPart");
 }
--- client/ships/myShips/myShipsList/myShipsList.js
+++ client/ships/myShips/myShipsList/myShipsList.js
@@ -1,7 +1,7 @@
 Template.myShipsList.helpers({
     myShips: function(){
         var page = parseInt(Session.get('currentPage'));
-        var limit = parseInt(Session.get('paginationLimit'));
+        var limit = parseInt(Session.get('myPaginationLimit'));
         return Ships.find({author:Meteor.user().username},{skip: (page-1)*limit, limit: limit});
 	}
 });
@@ -9,7 +9,7 @@ Template.myShipsList.helpers({
 
 Template.myShipsList.created = function(){
     var page = parseInt(Session.get('currentPage'));
-    var totalShips = Meteor.call("myShipsCount", function(error,result){ Session.set("paginationTotal",result);} );
-    Session.set("paginationLimit",2);
-    Session.set("paginationOf","Ship");
+    var totalShips = Meteor.call("myShipsCount", function(error,result){ Session.set("myPaginationTotal",result);} );
+    Session.set("myPaginationLimit",2);
+    Session.set("myPaginationOf","Ship");
 }
