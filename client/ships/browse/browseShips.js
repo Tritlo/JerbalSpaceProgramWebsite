312102fdc0a7b4d9ed1723e731618a787c82432f
solrun <solalola@gmail.com>
2013-11-29 21:48:45 +0000

    commenting ships

--- client/ships/browse/browseShips.js
+++ client/ships/browse/browseShips.js
@@ -1,5 +1,7 @@
 Template.browseShips.rendered = function (){
-        var id = Session.get('currentItem');
+    var id = Session.get('currentItem');
+	// We create an instance of the game which is 
+	// canvas to view ships on
 	bigViewer = start("Viewer",{
 	    instanceOptions : {
 		grid: {
@@ -12,20 +14,31 @@ Template.browseShips.rendered = function (){
 	    canvasID : "bigViewer",
 	    clear: false
 	    });
+		// The big canvas is the main instance of the game
+		// on the screen at the time
         Session.set("mainInstance",bigViewer);
+		// The ship displayed on the viewer is the current
+		// selected ship
         setShip(id,bigViewer);
 };
 
+// scope defines what parts we wants to see 
+// (i.e. only parts with current user as author)
+// empty brackets means all parts
 Template.browseShips.listData = {
     scope: function() {return {}}
 };
 
 Template.browseShips.events({
+	// when launch button is clicked the player goes 
+	// into gameplay mode with the selected ship
     "click .launch": function(evt){
 	evt.preventDefault();
 	var id = Session.get('currentItem');
 	if(id){Router.go('launchShip',{_id:id});}
     },
+	// when edit is clicked the player goes to the 
+	// ship designer and can modify selected ship
     "click .edit": function(evt){
 	evt.preventDefault();
 	var id = Session.get('currentItem');
--- client/ships/ship/ship.js
+++ client/ships/ship/ship.js
@@ -1,3 +1,5 @@
+// A template to view images of ships and select
+// them with the mouse
 Template.ship.events({
     "mouseenter" : function(event){
         var id = this._id;
@@ -9,15 +11,17 @@ Template.ship.events({
         setShip(id);
     },
     "click": function(event){
-	var id = this._id;
-	var page =Session.get('currentPage');
-	event.preventDefault();
-	Router.go(Router.current().route.name,{page: page,_id:id});
+		var id = this._id;
+		var page =Session.get('currentPage');
+		event.preventDefault();
+		Router.go(Router.current().route.name,{page: page,_id:id});
 	}
 });
 
 Template.ship.rendered = function(){
-         var shipId = this.data._id;
+	var shipId = this.data._id;
+	// We start an intance of the game which is 
+	// a canvas to view the ship on
 	 var shipViewer = start("Viewer",{
 	    instanceOptions : {
 		grid: {
@@ -35,6 +39,7 @@ Template.ship.rendered = function(){
         setShip(shipId,shipViewer);
 };
 
+// checks if ship's author is current user
 Template.ship.isMine = function(){
     return this.authorID === Meteor.userId();
 };
